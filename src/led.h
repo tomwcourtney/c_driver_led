48d369f60462fe765d1142e92242a14c5d573c3c
LAPTOP\xolop <thomas.courtney@roboticsystems.com.au>
2023-08-21 06:59:27 +1000

    Linked source to test. No tests added.

--- src/led.h
+++ src/led.h
@@ -1,10 +1,11 @@
-#define LED_H
-#ifndef LED_H
-
 /**
  * @file A driver for LEDs. They can be RGB or not and there can be 1 or many
  * LEDs. 
  */
+
+#ifndef LED_H
+#define LED_H
+
 
 
 
---
+++ test-harness/tests/led_test.cpp
@@ -0,0 +1,6 @@
+#include "CppUTest/TestHarness.h"
+
+extern "C" 
+{
+    #include "../../src/led.h"
+}
