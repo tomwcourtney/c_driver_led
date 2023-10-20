6b9ce662449be6db41060fd3469997fb0d66854f
RS_Lappy\Pat <patrick.kingwill@roboticsystems.com.au>
2023-10-20 13:02:00 +1100

    a

--- user_code/user_led.c
+++ user_code/user_led.c
@@ -4,8 +4,23 @@
 
 #include "led.h"
 #include <stdint.h>
-
+#include "main.h"
+extern TIM_HandleTypeDef htim2;
+extern TIM_HandleTypeDef htim21;
+extern TIM_HandleTypeDef htim22;
 void write(pins_t pins, led_state_t state)
 {
-    led_spy_set_state(pins.pin, state);
+//    led_spy_set_state(pins.pin, state);
+	if(pins.pin == 0) // red
+	{
+		TIM2->CCR3 = state;
+	}
+	else if (pins.pin == 1) // green
+	{
+		TIM22->CCR2 = state;
+	}
+	else if (pins.pin == 2) // blue
+	{
+		TIM22->CCR1 = state;
+	}
 }
